cbfcddc4917463469ae622514fb00d35cfe10791
Lzyct <hey.mudassir@gmail.com>
2022-06-20 23:18:46 +0800

    [CLEAN] Remove unused code

--- home/fish.nix
+++ home/fish.nix
@@ -1,14 +1,5 @@
 { config, pkgs, lib, ... }:
-let
-  shellAliases = with pkgs; {
-    ls = "${pkgs.exa}/bin/exa --color=auto --group-directories-first --classify --icons";
-    intel = "arch -x86_64";
-    fixaudio = "sudo launchctl stop com.apple.audio.coreaudiod && sudo launchctl start com.apple.audio.coreaudiod";
-    # Nix related
-    drb = "darwin-rebuild build --flake ~/.config/nixpkgs/";
-    drs = "darwin-rebuild switch --flake ~/.config/nixpkgs/";
-  };
-in
+
 {
   # Fish Shell (Default shell)
   # https://rycee.gitlab.io/home-manager/options.html#opt-programs.fish.enable
@@ -49,16 +40,6 @@ in
         sha256 = "069ybzdj29s320wzdyxqjhmpm9ir5815yx6n522adav0z2nz8vs4";
       };
     }
-    {
-      name = "thefuck";
-      src = pkgs.fetchFromGitHub
-        {
-          owner = "oh-my-fish";
-          repo = "plugin-thefuck";
-          rev = "6c9a926d045dc404a11854a645917b368f78fc4d";
-          sha256 = "1n6ibqcgsq1p8lblj334ym2qpdxwiyaahyybvpz93c8c9g4f9ipl";
-        };
-    }
   ];
 
   programs.fish.functions = {
@@ -69,49 +50,12 @@ in
   programs.fish.shellAbbrs = { };
 
   # Fish alias : register alias command in fish
-  programs.fish.shellAliases = shellAliases;
-
-  programs.fish.shellInit = ''
-    # TODO keybinding for thefuck
-    # Fish color
-    set -U fish_color_command 6CB6EB --bold
-    set -U fish_color_redirection DEB974
-    set -U fish_color_operator DEB974
-    set -U fish_color_end C071D8 --bold
-    set -U fish_color_error EC7279 --bold
-    set -U fish_color_param 6CB6EB
-    set fish_greeting
-  '';
-
-  # jump like `z` or `fasd`
-  programs.zoxide.enable = true;
-  programs.zoxide.enableBashIntegration = true;
-  programs.zoxide.enableZshIntegration = true;
-  programs.zoxide.enableFishIntegration = true;
-
-  # Fish prompt and style
-  programs.starship.enable = true;
-  programs.starship.settings = {
-    add_newline = true;
-    command_timeout = 1000;
-    cmd_duration = {
-      format = " [$duration]($style) ";
-      style = "bold #EC7279";
-      show_notifications = true;
-    };
-    nix_shell = {
-      format = " [$symbol$state]($style) ";
-    };
-    battery = {
-      full_symbol = "🔋 ";
-      charging_symbol = "⚡️ ";
-      discharging_symbol = "💀 ";
-    };
-    git_branch = {
-      format = "[$symbol$branch]($style) ";
-    };
-    gcloud = {
-      format = "[$symbol$active]($style) ";
-    };
+  programs.fish.shellAliases = {
+    ls = "${pkgs.exa}/bin/exa --color=auto --group-directories-first --classify --icons";
+    intel = "arch -x86_64";
+    fixaudio = "sudo launchctl stop com.apple.audio.coreaudiod && sudo launchctl start com.apple.audio.coreaudiod";
+    # Nix related
+    drb = "darwin-rebuild build --flake ~/.config/nixpkgs/";
+    drs = "darwin-rebuild switch --flake ~/.config/nixpkgs/";
   };
 }
