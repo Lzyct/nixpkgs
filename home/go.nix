0d21a26f8d834b97b8e08d3fa5609979ebece3f3
Lzyct <hey.mudassir@gmail.com>
2022-07-09 00:27:53 +0800

    Using latest go version 1.18.3

--- home/go.nix
+++ home/go.nix
@@ -1,7 +1,14 @@
-{ config, pkgs, lib, ... }:
+{ config, pkgs, lib,... }:
 
 {
    # Setup go
    programs.go.enable = true;
-
+   programs.go.package = with pkgs;go.overrideAttrs (oldAttrs: rec { 
+    # write new attr 
+    version = "1.18.3";
+    src = fetchurl {
+      url = "https://dl.google.com/go/go${version}.src.tar.gz";
+      sha256 = "sha256-ABI4bdy7XzNQ5AfGeZI4EdvSg/zcQhckkxYUqELsvC0=";
+    };
+  });
 }
