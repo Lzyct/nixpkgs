027c0dd542b4fd22aff9bfc1d0de62dd5cd5d9dd
Lzyct <hey.mudassir@gmail.com>
2022-07-14 21:18:03 +0800

    [FEAT] Update GoLang version 1.18.4

--- home/go.nix
+++ home/go.nix
@@ -5,10 +5,10 @@
    programs.go.enable = true;
    programs.go.package = with pkgs;go.overrideAttrs (oldAttrs: rec { 
     # write new attr 
-    version = "1.18.3";
+    version = "1.18.4";
     src = fetchurl {
       url = "https://dl.google.com/go/go${version}.src.tar.gz";
-      sha256 = "sha256-ABI4bdy7XzNQ5AfGeZI4EdvSg/zcQhckkxYUqELsvC0=";
+      sha256 = "sha256-RSWqaw487LV4RfQGCnB1qvyat1K7e2tM+KIS1DB44eQ=";
     };
   });
 }
