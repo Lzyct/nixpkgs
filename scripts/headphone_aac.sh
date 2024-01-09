57d3e855c274e5f722e12e50934976222946cd62
Lzyct <hey.mudassir@gmail.com>
2024-01-09 08:43:28 +0800

    feat: disable AptX codec

--- scripts/headphone_aac.sh
+++ scripts/headphone_aac.sh
@@ -11,6 +11,7 @@ defaults write com.apple.BluetoothAudioAgent "Stream - Max Outstanding Packets (
 defaults write com.apple.BluetoothAudioAgent "Stream Resume Delay" "0.75"
 
 defaults write bluetoothaudiod "Enable AAC codec" -bool true
+defaults write bluetoothaudiod "Enable AptX codec" -bool false
 defaults write bluetoothaudiod "AAC Bitrate" -int 320
 defaults write bluetoothaudiod "AAC max packet size" -int 644
 
