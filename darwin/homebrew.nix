c22ef2701d3617f61a4754865742bfc0e599d557
Lzyct <hey.mudassir@gmail.com>
2024-06-16 23:44:44 +0800

    feat: add alt-tab

--- darwin/homebrew.nix
+++ darwin/homebrew.nix
@@ -92,6 +92,7 @@ in
   # If an app isn't available in the Mac App Store, or the version in the App Store has
   # limitiations, e.g., Transmit, install the Homebrew Cask.
   homebrew.casks = [
+    "alt-tab"
     "airbuddy"
     "arc"
     "appcleaner"
