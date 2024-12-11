1c9da2d63d1b85d872a6c590a7d75f85276bc9b4
Lzyct <hey.mudassir@gmail.com>
2024-12-11 13:31:06 +0800

    chore: update deprecated build

--- darwin/defaults.nix
+++ darwin/defaults.nix
@@ -36,7 +36,7 @@
   # Dock and Mission Control
   system.defaults.dock = {
     autohide = true;
-    expose-group-by-app = false;
+    expose-group-apps = false;
     mru-spaces = false;
     tilesize = 38;
     # Disable all hot corners
